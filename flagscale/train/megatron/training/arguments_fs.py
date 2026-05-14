71057cf15b7fe175d50b1b19ebd1153ef1d6c034
LiJun <lijunx16@outlook.com>
2026-05-14 22:51:08 +0800

    [Train]: fix bug of engram when allreduce, and add example for traning 27B engram

--- flagscale/train/megatron/training/arguments_fs.py
+++ flagscale/train/megatron/training/arguments_fs.py
@@ -376,13 +376,13 @@ class FSTrainArguments:
             if self.args.engram_embedding_parallel_method == "allreduce":
                 if self.args.rank == 0:
                     warnings.warn(f"[rank0]: We do not recommend using allreduce for engram embedding, this is deprecated and will be removed in a later version.", DeprecationWarning)
-                    if self.args.engram_embedding_parallel_size is not None:
-                        warnings.warn(
-                            "[rank0]: If set the embedding_parallel_method to allreduce, " \
-                            "the embedding module will be the tensor_parallel.layers.VocabParallelEmbedding with tensor_parallel." \
-                            "So the embedding_parallel_size is useless and set to None."
-                        )
-                        self.args.engram_embedding_parallel_size = None   
+                if self.args.engram_embedding_parallel_size is not None:
+                    warnings.warn(
+                        "If set the embedding_parallel_method to allreduce, " \
+                        "the embedding module will be the tensor_parallel.layers.VocabParallelEmbedding with tensor_parallel." \
+                        "So the embedding_parallel_size is useless and set to None."
+                    )
+                    self.args.engram_embedding_parallel_size = None   
             elif self.args.engram_embedding_parallel_method == "alltoall":
                 assert self.args.engram_embedding_parallel_size is not None, "embedding parallel size should be specified when using alltoall"
             else:
